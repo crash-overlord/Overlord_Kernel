4124967dbcc442dcbdca51125b324be1c1e28995
crash-overlord <el_jose47@live.com>
2020-04-23 22:59:00 -0400

    added Chill and Relaxed governors

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Apr 23 20:42:04 AST 2020"
-#define LINUX_COMPILE_BY "Crash-Overlord"
-#define LINUX_COMPILE_HOST "Crash-Pc"
-#define LINUX_COMPILER "Android (6051079 based on r370808) clang version 10.0.1 (https://android.googlesource.com/toolchain/llvm-project b9738d6d99f614c8bf7a3e7c769659b313b88244)"
--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.4.219-Overlord-Kernel-V3-Beta-1-oldcam-gbd95f0d5ecb1"
